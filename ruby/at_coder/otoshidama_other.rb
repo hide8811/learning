f436c304954cc57e143afe2b2fb9bd23915c19b0
hide8811 <ikuyedih18@gmail.com>
2021-03-31 02:01:12 +0900

    Fix: [AtCoder] ABC085C - Otoshidama (別解)

--- ruby/at_coder/otoshidama_other.rb
+++ ruby/at_coder/otoshidama_other.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
-def total_amount(x, y, z)
-  (10_000 * x) + (5_000 * y) + (1_000 * z)
+def total_amount(x_num, y_num, z_num)
+  (10_000 * x_num) + (5_000 * y_num) + (1_000 * z_num)
 end
 
 n, yen = gets.split.map(&:to_i)
 
-x, y, z = 0, n, 0
+x = 0
+y = n
+z = 0
 
-while total_amount(x, y, z)
+while total_amount(x, y, z) != yen
   if y.zero?
-    x, y, z = -1, -1, -1
+    x = y = z = -1
     break
   end
 
   if total_amount(x, y, z) > yen
     y -= 1
-    x += 1
+    z += 1
 
   else
     y -= 1
