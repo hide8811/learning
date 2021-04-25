9cf96307ca230eb46f6f037c7e2860c7987ef242
hide8811 <ikuyedih18@gmail.com>
2021-04-25 22:16:03 +0900

    Improve: [Ruby: Calendar] コードを改善

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -2,51 +2,26 @@
 
 require 'Date'
 
-def centering(str)
-  str_count = str.length
-  space_count = (20 - str_count) / 2
-
-  space = ' ' * space_count
-
-  if str_count.even?
-    "┃#{space}#{str}#{space}┃"
-  else
-    "┃#{space}#{str}#{space} ┃"
-  end
-end
-
 today = Date.today
 
-# header
 year = today.strftime('%Y')
 month = today.strftime('%B')
+month_year = "#{month} #{year}"
 
-header_str = "#{month} #{year}"
+header_string = month_year.center(20)
 
 puts '┏━━━━━━━━━━━━━━━━━━━━┓'
-puts centering(header_str)
+puts "┃#{header_string}┃"
 puts '┣━━━━━━━━━━━━━━━━━━━━┫'
-
-# body
 puts '┃Su Mo Tu We Th Fr Sa┃'
 puts '┠────────────────────┨'
 
-first_day = Date.new(today.year, today.month, 1)
-end_day = (first_day >> 1) - 1
+first_date = Date.new(today.year, today.month, 1)
+last_date = (first_date >> 1) - 1
+date_array = (1..last_date.day).map { |day| format('%2d', day) }
 
-one_month = Array.new(first_day.wday, '  ') + (1..end_day.day).map { |day| day <= 9 ? " #{day}" : day }
+one_month = Array.new(first_date.wday, '  ') + date_array + Array.new(6 - last_date.wday, '  ')
 
-month_weeks = one_month.each_slice(7).to_a
+one_month.each_slice(7) { |week| puts "┃#{week.join(' ')}┃" }
 
-month_weeks.map do |week|
-
-  if week.length == 7
-    print "┃#{week.join(' ')}┃\n"
-  else
-    last_week = week + Array.new((7 - week.length), '  ')
-    print "┃#{last_week.join(' ')}┃\n"
-    puts '┗━━━━━━━━━━━━━━━━━━━━┛'
-  end
-end
-
-
+puts '┗━━━━━━━━━━━━━━━━━━━━┛'
