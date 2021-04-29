947b84b443bef91f160b6f59e4d48aee809fcc00
hide8811 <ikuyedih18@gmail.com>
2021-04-29 21:59:07 +0900

    Add: [AtCoder] ABC199C - IPFL

---
+++ ruby/at_coder/ipfl.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+n = gets.to_i
+s = gets.chomp
+q = gets.to_i
+
+s_first_half, s_second_half = s.chars.each_slice(n).to_a
+
+q.times do
+  t, a, b = gets.split.map(&:to_i)
+
+  if t == 1
+    a_ary = a <= n ? s_first_half : s_second_half
+    b_ary = b <= n ? s_first_half : s_second_half
+
+    a -= a <= n ? 1 : (1 + n)
+    b -= b <= n ? 1 : (1 + n)
+
+    a_ary[a], b_ary[b] = b_ary[b], a_ary[a]
+  else
+    s_first_half, s_second_half = s_second_half, s_first_half
+  end
+end
+
+puts [s_first_half, s_second_half].join
