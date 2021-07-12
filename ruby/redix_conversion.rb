c9d3450cef3000359b6cd12b279d1791bb1cd2a1
hide8811 <ikuyedih18@gmail.com>
2021-07-12 22:31:36 +0900

    Add: [Ruby: Redix conversion] 2進数 -> 10進数 計算(小数点あり)

--- ruby/redix_conversion.rb
+++ ruby/redix_conversion.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'bigdecimal'
+
 # クラスメソッドを追加
 class Integer
   def ten?
@@ -14,7 +16,7 @@ end
 print '基数:'
 original_radix = gets.to_i
 print '数値:'
-number = gets.to_i
+number = gets.chomp
 print '変換する基数:'
 after_radix = gets.to_i
 
@@ -24,13 +26,45 @@ after_number_a =
   if original_radix == after_radix
     number
   elsif original_radix.ten? && after_radix.not_ten?
-    number.to_s(after_radix)
+    number.to_i.to_s(after_radix)
   elsif original_radix.not_ten? && after_radix.ten?
-    number.to_s.to_i(original_radix)
+    number.to_i(original_radix)
   else
-    tmp = number.to_s.to_i(original_radix)
+    tmp = number.to_i(original_radix)
     tmp.to_s(after_radix)
   end
 
 puts # 空行
 puts "結果: #{after_number_a}"
+# ===============================================
+
+# 計算使用 ======================================
+if original_radix == after_radix
+  puts number
+  exit
+end
+
+n_ary = number.split('')
+point_index = n_ary.index('.')
+
+integral = n_ary[0..(point_index - 1)]
+decimal = n_ary[(point_index + 1)..-1]
+
+after_number_b =
+  if original_radix.not_ten? && after_radix.ten? && point_index
+    integral_sum =
+      integral.reverse.each_with_index.inject(0) { |sum, (elem, i)| sum + (elem.to_i * original_radix**i) }
+
+    decimal_sum =
+      # inject の 0 でエラーが出てしまうため、{} を使用
+      decimal.each_with_index.inject(0) { |sum, (elem, i)|
+        product = BigDecimal(elem) * BigDecimal(original_radix.to_s)**-BigDecimal((i + 1).to_s)
+        (BigDecimal(sum.to_s) + BigDecimal(product.to_s)).to_f
+      }
+
+    integral_sum + decimal_sum
+  end
+
+puts # 空行
+puts "結果: #{after_number_b}"
+# ===============================================
