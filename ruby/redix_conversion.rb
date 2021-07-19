263f94cf581a8eabafc4da251166a33e8a80008d
hide8811 <ikuyedih18@gmail.com>
2021-07-19 17:00:00 +0900

    Improve:  [Ruby: Redix conversion] 共通処理をメソッド化

--- ruby/redix_conversion.rb
+++ ruby/redix_conversion.rb
@@ -13,12 +13,27 @@ class Integer
   end
 end
 
-def integral_n_to_ten(integral_ary, radix)
-  integral_ary.reverse.each_with_index.inject(0) { |sum, (elem, i)| sum + (elem.to_i * radix**i) }
+# クラスメソッドを追加
+class Float
+  def disassembly
+    int, dec = to_s.split('.')
+    zero_dec = to_d - int.to_d
+
+    # self = 1.2
+    # ['1', '0.2', '2']
+    [int, zero_dec, dec]
+  end
 end
 
-def decimal_n_to_ten(decimal_ary, radix)
-  decimal_ary.each_with_index.inject(0) do |sum, (elem, i)|
+def integral_n_to_ten(integral, radix)
+  ary = integral.split('').reverse
+  ary.each_with_index.inject(0) { |sum, (elem, i)| sum + (elem.to_i * radix**i) }
+end
+
+def decimal_n_to_ten(decimal, radix)
+  ary = decimal.split('')
+
+  ary.each_with_index.inject(0) do |sum, (elem, i)|
     product = elem.to_d * radix.to_d**-(i + 1).to_d
 
     (sum.to_d + product.to_d).to_f
@@ -41,10 +56,8 @@ def decimal_ten_to_n(decimal, radix)
   dec = ''
 
   until decimal.zero?
-    decimal = (decimal.to_d * radix.to_d).to_f
-    i = decimal.to_i.to_s
-    dec += i
-    decimal = (decimal.to_d - i.to_d).to_f
+    int, decimal, = (decimal.to_d * radix.to_d).to_f.disassembly
+    dec += int.to_s
   end
 
   dec
@@ -53,21 +66,24 @@ end
 print '基数:'
 original_radix = gets.to_i
 print '数値:'
-number = gets.chomp
+number = gets.to_f
 print '変換する基数:'
 after_radix = gets.to_i
 
 # to_s to_i メソッド使用 ========================
 # 数値は整数のみ、基数は0, 2 〜 36 まで
 after_number_a =
-  if original_radix == after_radix
+  if original_radix == after_radix || number.zero?
     number
   elsif original_radix.ten? && after_radix.not_ten?
-    number.to_i.to_s(after_radix)
+    num = number.to_i
+    num.to_s(after_radix)
   elsif original_radix.not_ten? && after_radix.ten?
-    number.to_i(original_radix)
+    num = number.to_i
+    num.to_s.to_i(original_radix)
   else
-    tmp = number.to_i(original_radix)
+    num = number.to_i
+    tmp = num.to_s.to_i(original_radix)
     tmp.to_s(after_radix)
   end
 
@@ -76,52 +92,42 @@ puts "結果: #{after_number_a}"
 # ===============================================
 
 # 計算使用 ======================================
-if original_radix == after_radix || number == '0'
+# 基数は10まで
+if original_radix == after_radix || number.zero?
   puts "結果: #{number}"
   exit
 end
 
-n_ary = number.split('')
-point_index = n_ary.index('.')
+integral, zero_dec, decimal = number.disassembly
+
+# n_ary = number.split('')
+# point_index = n_ary.index('.')
 
 after_number_b =
   if original_radix.not_ten? && after_radix.ten?
-    if point_index
-      integral_ary = n_ary[0..(point_index - 1)]
-      decimal_ary = n_ary[(point_index + 1)..-1]
-
-      integral_n_to_ten(integral_ary, original_radix) + decimal_n_to_ten(decimal_ary, original_radix)
+    if decimal.to_i.zero?
+      integral_n_to_ten(integral, original_radix)
     else
-      integral_n_to_ten(n_ary, original_radix)
+      integral_n_to_ten(integral, original_radix) + decimal_n_to_ten(decimal, original_radix)
     end
-
   elsif original_radix.ten? && after_radix.not_ten?
-    if point_index
-      integral = number.to_i
-      decimal = (number.to_d - integral.to_d).to_f
-
-      "#{integral_ten_to_n(integral, after_radix)}.#{decimal_ten_to_n(decimal, after_radix)}"
+    if decimal.to_i.zero?
+      integral_ten_to_n(integral, after_radix)
     else
-      number = number.to_i
-
-      integral_ten_to_n(number, after_radix)
+      "#{integral_ten_to_n(integral.to_i, after_radix)}.#{decimal_ten_to_n(zero_dec.to_f, after_radix)}"
     end
 
   # original_radix.not_ten? && after_radix.not_ten?
-  elsif point_index
-    integral_ary = n_ary[0..(point_index - 1)]
-    decimal_ary = n_ary[(point_index + 1)..-1]
-
-    num = integral_n_to_ten(integral_ary, original_radix) + decimal_n_to_ten(decimal_ary, original_radix)
-
-    integral = num.to_i
-    decimal = (num.to_d - integral.to_d).to_f
-
-    "#{integral_ten_to_n(integral, after_radix)}.#{decimal_ten_to_n(decimal, after_radix)}"
-  else
-    num = integral_n_to_ten(n_ary, original_radix)
+  elsif decimal.to_i.zero?
+    num = integral_n_to_ten(integral, original_radix)
 
     integral_ten_to_n(num, after_radix)
+  else
+    num = (integral_n_to_ten(integral, original_radix).to_d + decimal_n_to_ten(decimal, original_radix).to_d).to_f
+
+    int, z_dec, = num.disassembly
+
+    "#{integral_ten_to_n(int.to_i, after_radix)}.#{decimal_ten_to_n(z_dec.to_f, after_radix)}"
   end
 
 puts # 空行
