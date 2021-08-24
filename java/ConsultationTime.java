62f1cad2438837e02eac2410f5a37152ff009980
hide8811 <ikuyedih18@gmail.com>
2021-08-24 20:56:03 +0900

    Add: [Java: Consultation Time] 現在の曜日・時刻の判断機能を追加

--- java/ConsultationTime.java
+++ java/ConsultationTime.java
@@ -1,73 +1,91 @@
 /*
 |      | 日曜 | 月曜 | 火曜 | 水曜 | 木曜 | 金曜 | 土曜 |
 |:----:|:----:|:----:|:----:|:----:|:----:|:----:|:----:|
-| 午前 | 休診 |  ◯   | 休診 |  ◯   |  ◯   | 休診 |  ◯   |
-| 午後 | 休診 |  ◯   |  ◯   |  ◯   |  ◯   |  ◯   | 休診 |
+| 午前 | 休診 |  ◯   | 休診 |  ◯   |  ◯   | 休診 |  ◯   | 8:00 ~ 13:00
+| 午後 | 休診 |  ◯   |  ◯   |  ◯   |  ◯   |  ◯   | 休診 | 13:00 ~ 18:00
 | 夜間 | 休診 |  ◯   |  ◯   | 休診 |  ◯   |  ◯   | 休診 |
 */
 
 import java.util.Scanner;
+import java.util.Calendar;
 
 public class ConsultationTime {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    Calendar calendar = Calendar.getInstance();
 
-    String weekdayList = "[0]: 日曜、[1]: 月曜、[2]: 火曜、[3]: 水曜、[4]: 木曜、[5]: 金曜、[6]: 土曜";
-    String timeframeList = "[0]: 午前、[1]: 午後、[2]: 夜間";
+    String weekdayList = "[0]:本日  [1]:日曜  [2]:月曜  [3]:火曜  [4]:水曜  [5]:木曜  [6]:金曜  [7]:土曜";
+    String timeframeList = "[0]:現在  [1]:午前(8:00 ~ 13:00)  [2]:午後(13:00 ~ 18:00)  [3]:夜間";
 
     System.out.println("曜日を入力してください");
     System.out.println(weekdayList);
     int weekday = scanner.nextInt();
 
-    while (weekday < 0 || 6 < weekday) {
-      System.out.println("0 〜 6の数値で入力してください");
+    while (weekday < 0 || 7 < weekday) {
+      System.out.println("0 〜 7の数値で入力してください");
       weekday = scanner.nextInt();
     }
+
+    if (weekday == 0) weekday = calendar.get(Calendar.DAY_OF_WEEK);
+
+    int ca = calendar.get(Calendar.AM_PM);
 
     System.out.println("時間帯を入力してください");
     System.out.println(timeframeList);
     int timeframe = scanner.nextInt();
 
-    while (timeframe < 0 || 2 < timeframe) {
-      System.out.println("0 〜 2の数値で入力してください");
+    while (timeframe < 0 || 3 < timeframe) {
+      System.out.println("0 〜 3の数値で入力してください");
       timeframe = scanner.nextInt();
+    }
+
+    if (timeframe == 0) {
+      int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+      if (8 <= hour && hour < 13) {
+        timeframe = 1;
+      } else if (13 <= hour && hour < 18) {
+        timeframe = 2;
+      } else {
+        timeframe = 3;
+      }
     }
 
     System.out.println("----------------------");
 
     switch(weekday) {
-      case 0:
+      case 1:
         System.out.println("休診日です");
         break;
-      case 1:
+      case 2:
         System.out.println("受診可能です");
         break;
-      case 2:
-        if (timeframe == 0) {
-          System.out.println("休診日です");
-        } else {
-          System.out.println("受診可能です");
-        }
-        break;
       case 3:
-        if (timeframe == 2) {
+        if (timeframe == 1) {
           System.out.println("休診日です");
         } else {
           System.out.println("受診可能です");
         }
         break;
       case 4:
-        System.out.println("受診可能です");
-        break;
-      case 5:
-        if (timeframe == 0) {
+        if (timeframe == 3) {
           System.out.println("休診日です");
         } else {
           System.out.println("受診可能です");
         }
         break;
+      case 5:
+        System.out.println("受診可能です");
+        break;
       case 6:
-        if (timeframe == 0) {
+        if (timeframe == 1) {
+          System.out.println("休診日です");
+        } else {
+          System.out.println("受診可能です");
+        }
+        break;
+      case 7:
+        if (timeframe == 1) {
           System.out.println("受診可能です");
         } else {
           System.out.println("休診日です");
